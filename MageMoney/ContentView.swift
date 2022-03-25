1dd2177fd682501a4f855b099bf67b7c40978604
Benderson Phanor <sonderben@gmail.com>
2022-03-25 18:59:12 -0400

    Initial Commit

---
+++ MageMoney/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MageMoney
+//
+//  Created by Benderson Phanor on 25/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MageMoney/MageMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MageMoneyApp.swift
+//  MageMoney
+//
+//  Created by Benderson Phanor on 25/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MageMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
